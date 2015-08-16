9fce3fcb4e1070271e012e14a7f0e3565c6cdb7b
Loïc MARIE <loic.marie54@gmail.com>
2015-08-16 18:14:26 +0200

    Creation of rules files for pieces

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 var Chessboard = require('./lib/chessboard.js'),
 	chessboard = new Chessboard();
+	
+chessboard.play('Pe3');
+chessboard.play('pe6');
+	
+console.log(chessboard.get());
+
+// chessboard.play('Bf3');
+
+// console.log(chessboard.get());
